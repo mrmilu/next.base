4930b6a4bab06a435809091137b21de1ddc9c872
Arnau Gómez <agomez@mrmilu.com>
2022-04-27 08:37:59 +0200

    feat(carousel): create carousel example page

---
+++ src/ui/features/dummy/components/carousel_page/carousel_page.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CarouselPageStyled = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin: 40px auto;
+`;
+
+export const CarouselSlideStyled = styled.article`
+  height: 500px;
+  background-color: white;
+`;
