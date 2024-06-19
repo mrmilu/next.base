51baf43ca4273bfc7d7fc8001c26ec20c6d77bf1
Arnau <agomez@mrmilu.com>
2024-06-19 09:13:51 +0200

    fix: next js build error https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- src/ui/components/base_layout/base_layout.tsx
+++ src/ui/components/base_layout/base_layout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
-import React from "react";
+import React, { Suspense } from "react";
 import css from "./base_layout.css";
 import LoginButton from "@/src/ui/features/misc/containers/login_button/login_button";
 import { Link } from "@/src/ui/i18n";
@@ -23,7 +23,9 @@ export const BaseLayout = ({ children }: PropsWithChildren) => {
           <ListElement href="/create_post" label="create post" />
           <ListElement href="/posts" label="list post" />
         </ul>
-        <LoginButton />
+        <Suspense>
+          <LoginButton />
+        </Suspense>
       </nav>
       <main className={css.main}>{children}</main>
       <footer className={css.footer}>cool footer</footer>
