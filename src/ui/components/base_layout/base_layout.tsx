c63de0bd57c51b00e30f52e30089a7bd2c051094
Arnau <agomez@mrmilu.com>
2024-06-19 08:41:10 +0200

    fix: remove useless Suspense component

--- src/ui/components/base_layout/base_layout.tsx
+++ src/ui/components/base_layout/base_layout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
-import React, { Suspense } from "react";
+import React from "react";
 import css from "./base_layout.css";
 import LoginButton from "@/src/ui/features/misc/containers/login_button/login_button";
 import { Link } from "@/src/ui/i18n";
@@ -23,9 +23,7 @@ export const BaseLayout = ({ children }: PropsWithChildren) => {
           <ListElement href="/create_post" label="create post" />
           <ListElement href="/posts" label="list post" />
         </ul>
-        <Suspense>
-          <LoginButton />
-        </Suspense>
+        <LoginButton />
       </nav>
       <main className={css.main}>{children}</main>
       <footer className={css.footer}>cool footer</footer>
